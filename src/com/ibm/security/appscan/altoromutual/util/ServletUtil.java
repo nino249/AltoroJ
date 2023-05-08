865dbaa0e87dc2dcb37368a2a68f416a7df1852c
nino249 <131352348+nino249@users.noreply.github.com>
2023-05-08 11:02:07 -0400

    Update ServletUtil.java

--- src/com/ibm/security/appscan/altoromutual/util/ServletUtil.java
+++ src/com/ibm/security/appscan/altoromutual/util/ServletUtil.java
@@ -350,6 +350,7 @@ public class ServletUtil {
 			accountCookie.setSecure(false);
 			accountCookie.setSecure(false);
 			accountCookie.setSecure(false);
+			accountCookie.setSecure(false);
 		    return accountCookie;
 		}
 		catch(SQLException e){
